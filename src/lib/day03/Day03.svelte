1130a5b7f093a5a767985d509a44ad367e5faad5
Razvan <>
2022-12-04 10:20:23 +0100

    feat: Day 3 extara - scrolling number -> thanks to Geoff Rich

--- src/lib/day03/Day03.svelte
+++ src/lib/day03/Day03.svelte
@@ -1,77 +1,106 @@
 <script lang="ts">
   import Problem from "../Problem.svelte";
   import type { Countdown } from "./types";
-
+  import Rolling from "./Rolling.svelte";
   const christmas_day = (date: Date): Date => {
-    let year = (date.getMonth() == 11 && date.getDate() >= 25) ? date.getFullYear() + 1 : date.getFullYear()
-    return new Date(year, 11, 25)
-  }
+    let year =
+      date.getMonth() == 11 && date.getDate() >= 25
+        ? date.getFullYear() + 1
+        : date.getFullYear();
+    return new Date(year, 11, 25);
+  };
 
   const calc_remain = (date: Date): Countdown => {
-    let seconds = Math.floor((christmas_day(date).getTime() - date.getTime())/1000)
-    let minutes = Math.floor(seconds/60)
-    let hours = Math.floor(minutes/60)
-    let days = Math.floor(hours/24)
+    let seconds = Math.floor(
+      (christmas_day(date).getTime() - date.getTime()) / 1000
+    );
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
 
-    hours = hours - (days * 24)
-    minutes = minutes - (days * 24 * 60)-(hours * 60)
-    seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
-    return {days, hours, minutes, seconds}
-  }
-  let { days, hours, minutes, seconds } = calc_remain(new Date()) 
+    hours = hours - days * 24;
+    minutes = minutes - days * 24 * 60 - hours * 60;
+    seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+    return { days, hours, minutes, seconds };
+  };
+  let { days, hours, minutes, seconds } = calc_remain(new Date());
 
-  const reload = () => setTimeout(() => {
-    const countdown = calc_remain(new Date())
-    days = countdown.days
-    hours = countdown.hours
-    minutes = countdown.minutes
-    seconds = countdown.seconds
-    reload()
-  }, 1000)
+  const reload = () =>
+    setTimeout(() => {
+      const countdown = calc_remain(new Date());
+      days = countdown.days;
+      hours = countdown.hours;
+      minutes = countdown.minutes;
+      seconds = countdown.seconds;
+      reload();
+    }, 1000);
 
-  reload()
+  reload();
 </script>
 
 <div class="wrapper">
   <div class="title">
     <Problem title="Day 3">
-      Christmas is coming soon! But how soon? 🤔 
-      Wouldn't it be nice to know exactly how many days, hours, minutes, and seconds are left until that magical day? 
-      In this challenge, you'll create a countdown to tell you exactly that.
+      Christmas is coming soon! But how soon? 🤔 Wouldn't it be nice to know
+      exactly how many days, hours, minutes, and seconds are left until that
+      magical day? In this challenge, you'll create a countdown to tell you
+      exactly that.
       <ul>
-        <li>Use the useNow composable from VueUse (preinstalled) to get a reactive version of the current time and then do the math to get days, hours, minutes, and seconds until Christmas.</li>
-        <li>If you're up for a bonus challenge use the Vue transition component to transition smoothly between each countdown number! 🚀</li>
+        <li>
+          Use the useNow composable from VueUse (preinstalled) to get a reactive
+          version of the current time and then do the math to get days, hours,
+          minutes, and seconds until Christmas.
+        </li>
+        <li>
+          If you're up for a bonus challenge use the Vue transition component to
+          transition smoothly between each countdown number! 🚀
+        </li>
       </ul>
     </Problem>
   </div>
   <div class="line column white">
-  <div class="title">
-    <h5>Christmas Countdown</h5>
-  </div>
-  <div class="column">
-    <div class="grid-container">
-      <div class="grid-item">
-        <span class="count">{days}</span><br/>
-        <span class="text">days</span>
-      </div>
-      <div class="grid-item">
-        <span class="count">{hours}</span><br/>
-        <span class="text">hours</span>
-      </div>
-      <div class="grid-item">
-        <span class="count">{minutes}</span><br/>
-        <span class="text">minutes</span>
-        </div>
-      <div class="grid-item">
-        <span class="count">{seconds}</span><br/>
-        <span class="text">seconds</span>
-      </div>  
+    <div class="title">
+      <h5>Christmas Countdown</h5>
+    </div>
+    <div class="column">
+      <d1>
+        <dt>Days</dt>
+        <Rolling key={days} --area="days">{days}</Rolling>
+        <dt>Hours</dt>
+        <Rolling key={hours} --area="hours">{hours}</Rolling>
+        <dt>Minutes</dt>
+        <Rolling key={minutes} --area="minutes">{minutes}</Rolling>
+        <dt>Seconds</dt>
+        <Rolling key={seconds} --area="seconds">{seconds}</Rolling>
+      </d1>
     </div>
   </div>
 </div>
-</div>
 
 <style>
+  d1 {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-areas: "days hours minutes seconds";
+    text-transform: lowercase;
+    gap: 0.5rem;
+    overflow: hidden;
+    text-align: center;
+    background-color: white;
+    margin: auto;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  dt {
+    grid-row: 2;
+    z-index: 1;
+    margin: auto;
+    background: white;
+    padding-bottom: 5px;
+  }
+
   .white {
     background-color: rgb(240, 242, 244);
     width: 50% !important;
@@ -89,14 +118,6 @@
     flex-direction: column;
     margin: auto;
   }
-  .count {
-    color: rgb(66 184 131);
-    font-size: larger;
-    font-weight: bolder;
-  }
-  .text {
-    font-size: smaller;
-  }
   .title {
     flex: 100%;
     font-size: x-large;
@@ -109,18 +130,4 @@
     justify-content: center;
     font-size: x-large;
   }
-
-.grid-item {
-  background-color: rgb(255, 255, 255);
-    color: black;
-    padding: 15px;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
-
-.grid-container {
-  display: grid;
-  grid-auto-flow: column;
-  grid-auto-columns: 1fr;
-}
-
 </style>
---
+++ src/lib/day03/Rolling.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+
+  export let key: number;
+
+  const distance = 50;
+  const duration = 700;
+
+  const commun = { duration, opacity: 1 };
+</script>
+
+{#key key}
+  <dd
+    in:fly|local={{ y: -1 * distance, ...commun }}
+    out:fly|local={{ y: distance, ...commun }}
+  >
+    <slot />
+  </dd>
+{/key}
+
+<style>
+  dd {
+    grid-area: var(--area);
+    overflow: hidden;
+    display: block;
+    color: rgb(66 184 131);
+    font-size: larger;
+    font-weight: bolder;
+    text-align: center;
+    margin: auto;
+    padding-top: 10px;
+  }
+</style>
